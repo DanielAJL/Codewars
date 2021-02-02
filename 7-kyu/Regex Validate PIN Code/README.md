c70e45bf9ede9163ccdde9263a34614fd544a860
Daniel Lynch <daniellynch.dev@gmail.com>
2021-02-02 15:33:08 +0100

    added kata: Regex Validate PIN Code

---
+++ 7-kyu/Regex Validate PIN Code/README.md
@@ -0,0 +1,16 @@
+# Description:
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+<br><br>
+
+# Task:
+If the function is passed a valid PIN string, return `true`, else return `false`.
+
+<br>
+
+# Examples:
+``` 
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+```
+
---
+++ 7-kyu/Regex Validate PIN Code/main.js
@@ -0,0 +1,15 @@
+const validatePIN = (str) => {
+  const isValid = (str.length === 4 || str.length === 6) && /^[0-9]+$/.test(str) ? true : false;
+  return isValid;
+}
+
+
+// test cases
+validatePIN("123456") // true
+validatePIN("1234") // true
+validatePIN("090909") // true
+validatePIN("1.234") // false
+validatePIN("1") // false
+validatePIN("a234") // false
+validatePIN("-1234") // false
+validatePIN("-34+") // false
