f150b514445c6ba97ce58813a8fdea2353afbef7
Daniel Lynch <daniellynch.dev@gmail.com>
2021-02-09 19:20:12 +0100

    refactored code

--- 7-kyu/Regex Validate PIN Code/main.js
+++ 7-kyu/Regex Validate PIN Code/main.js
@@ -1,15 +1,14 @@
 const validatePIN = (str) => {
-  const isValid = (str.length === 4 || str.length === 6) && /^[0-9]+$/.test(str) ? true : false;
+  const isValid = !!((str.length === 4 || str.length === 6) && /^[0-9]+$/.test(str));
   return isValid;
-}
-
+};
 
 // test cases
-validatePIN("123456") // true
-validatePIN("1234") // true
-validatePIN("090909") // true
-validatePIN("1.234") // false
-validatePIN("1") // false
-validatePIN("a234") // false
-validatePIN("-1234") // false
-validatePIN("-34+") // false
+validatePIN('123456'); // true
+validatePIN('1234'); // true
+validatePIN('090909'); // true
+validatePIN('1.234'); // false
+validatePIN('1'); // false
+validatePIN('a234'); // false
+validatePIN('-1234'); // false
+validatePIN('-34+'); // false
